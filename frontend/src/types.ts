434a51855d86083a2ef4cee7f12a5e5087777b13
Vladimir Brankov <brankovv@protonmail.com>
2025-11-22 12:10:02 -0500

    .

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -9,7 +9,7 @@ export interface Sentence {
 export interface Lesson {
   id: number;
   title: string;
-  words: string[];
+  words: string[] | Array<{ english: string; serbian: string }>;
   sentences: Sentence[];
 }
 
